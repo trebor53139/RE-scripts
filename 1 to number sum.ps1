e577c7f19d0720de946cb99876ad4a89ee50d798
trebor53139 <84404905+trebor53139@users.noreply.github.com>
2025-04-08 23:22:12 -0500

    Add files via upload

---
+++ 1 to number sum.ps1
@@ -0,0 +1,20 @@
+﻿do {
+    do {
+        Write-Host "Please enter a positive number"
+        [int]$number = Read-Host
+    } while ($number -le 0)
+
+    # Calculation
+    [int]$sum = ($number * ($number + 1)) / 2
+    Write-Host "The sum of 1 to $number is $sum"
+
+    # Ask if the user wants to run the script again
+    do {
+        Write-Host "Do you want to run the script again? (1 for yes, 2 for no)"
+        [int]$choice = Read-Host
+    } while ($choice -ne 1 -and $choice -ne 2)
+
+} while ($choice -eq 1)
+
+Write-Host "Script ended. Have a great day!"
+
